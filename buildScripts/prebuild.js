45aac5f9c1ac84a268be02dfc2b26b0bb34b94c5
unknown <Tim.Smith@notamac.i3domain.inin.com>
2016-01-06 11:07:08 -0700

    updates

--- buildScripts/prebuild.js
+++ buildScripts/prebuild.js
@@ -1,5 +1,6 @@
 var wget = require('wget');
 var mkdirp = require('mkdirp');
+var fs = require('fs');
 var src = 'http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.1.4/swagger-codegen-cli-2.1.4.jar';
 var output = 'bin/swagger-codegen-cli.jar';
 var progressTracker = 0;
@@ -19,6 +20,7 @@ mkdirp('bin', function(err) {
 	});
 	download.on('end', function(output) {
 	    console.log('File downloaded to : ' + output);
+	    writeConfig();
 	});
 	download.on('progress', function(progress) {
 		// Report progress at 5% intervals
@@ -29,3 +31,35 @@ mkdirp('bin', function(err) {
 		}
 	});
 });
+
+function writeConfig() {
+	var version = "0.0.0.0";
+	process.argv.forEach(function (val, index, array) {
+		console.log(index + ': ' + val);
+		if (stringStartsWith(val, '/version=')) {
+			version = val.substring(9);
+			console.log('version = ' + version);
+		}
+	});
+
+	var config = {
+		"packageName":"ININ.PureCloudApi",
+		"packageVersion":version,
+		"packageTitle":"PureCloud Public API Library",
+	    "packageProductName":"PureCloudPublicAPILibrary",
+	    "packageDescription":"A .NET library to interface with the PureCloud Public API",
+	    "packageCompany":"Interactive Intelligence, Inc.",
+	    "packageCopyright":"Copyright © Interactive Intelligence, Inc. 2015"
+	};
+
+	fs.writeFile('bin/config-csharp.json', JSON.stringify(config), function(err) {
+		if (err) {
+			console.log('Failed to write config file!');
+			console.log(err);
+		}
+	});
+}
+
+function stringStartsWith (string, prefix) {
+    return string.slice(0, prefix.length) == prefix;
+}
