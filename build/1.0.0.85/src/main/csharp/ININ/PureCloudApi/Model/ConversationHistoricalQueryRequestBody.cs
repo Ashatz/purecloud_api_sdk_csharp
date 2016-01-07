490d7bc2b99da980119b8dd52f0cae28007a02c0
InIn Devops <devops-cloudapplications@inin.com>
2016-01-07 02:59:07 -0500

    1.0.0.85

---
+++ build/1.0.0.85/src/main/csharp/ININ/PureCloudApi/Model/ConversationHistoricalQueryRequestBody.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace ININ.PureCloudApi.Model
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class ConversationHistoricalQueryRequestBody :  IEquatable<ConversationHistoricalQueryRequestBody>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConversationHistoricalQueryRequestBody" /> class.
+        /// </summary>
+        public ConversationHistoricalQueryRequestBody()
+        {
+            
+        }
+
+        
+        /// <summary>
+        /// Gets or Sets PageSize
+        /// </summary>
+        [DataMember(Name="pageSize", EmitDefaultValue=false)]
+        public int? PageSize { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Maximum
+        /// </summary>
+        [DataMember(Name="maximum", EmitDefaultValue=false)]
+        public int? Maximum { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Filters
+        /// </summary>
+        [DataMember(Name="filters", EmitDefaultValue=false)]
+        public List<ConversationHistoricalQueryRequestFilter> Filters { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Facets
+        /// </summary>
+        [DataMember(Name="facets", EmitDefaultValue=false)]
+        public List<string> Facets { get; set; }
+  
+        
+  
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class ConversationHistoricalQueryRequestBody {\n");
+            sb.Append("  PageSize: ").Append(PageSize).Append("\n");
+            sb.Append("  Maximum: ").Append(Maximum).Append("\n");
+            sb.Append("  Filters: ").Append(Filters).Append("\n");
+            sb.Append("  Facets: ").Append(Facets).Append("\n");
+            
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            return this.Equals(obj as ConversationHistoricalQueryRequestBody);
+        }
+
+        /// <summary>
+        /// Returns true if ConversationHistoricalQueryRequestBody instances are equal
+        /// </summary>
+        /// <param name="obj">Instance of ConversationHistoricalQueryRequestBody to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(ConversationHistoricalQueryRequestBody other)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            if (other == null)
+                return false;
+
+            return 
+                (
+                    this.PageSize == other.PageSize ||
+                    this.PageSize != null &&
+                    this.PageSize.Equals(other.PageSize)
+                ) && 
+                (
+                    this.Maximum == other.Maximum ||
+                    this.Maximum != null &&
+                    this.Maximum.Equals(other.Maximum)
+                ) && 
+                (
+                    this.Filters == other.Filters ||
+                    this.Filters != null &&
+                    this.Filters.SequenceEqual(other.Filters)
+                ) && 
+                (
+                    this.Facets == other.Facets ||
+                    this.Facets != null &&
+                    this.Facets.SequenceEqual(other.Facets)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks etc, of course :)
+                
+                if (this.PageSize != null)
+                    hash = hash * 57 + this.PageSize.GetHashCode();
+                
+                if (this.Maximum != null)
+                    hash = hash * 57 + this.Maximum.GetHashCode();
+                
+                if (this.Filters != null)
+                    hash = hash * 57 + this.Filters.GetHashCode();
+                
+                if (this.Facets != null)
+                    hash = hash * 57 + this.Facets.GetHashCode();
+                
+                return hash;
+            }
+        }
+
+    }
+
+
+}
---
+++ build/1.0.0.85/src/main/csharp/ININ/PureCloudApi/Model/ValidateAddressResponse.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace ININ.PureCloudApi.Model
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class ValidateAddressResponse :  IEquatable<ValidateAddressResponse>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidateAddressResponse" /> class.
+        /// </summary>
+        public ValidateAddressResponse()
+        {
+            this.Valid = false;
+            
+        }
+
+        
+        /// <summary>
+        /// Gets or Sets Valid
+        /// </summary>
+        [DataMember(Name="valid", EmitDefaultValue=false)]
+        public bool? Valid { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Response
+        /// </summary>
+        [DataMember(Name="response", EmitDefaultValue=false)]
+        public Response Response { get; set; }
+  
+        
+  
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class ValidateAddressResponse {\n");
+            sb.Append("  Valid: ").Append(Valid).Append("\n");
+            sb.Append("  Response: ").Append(Response).Append("\n");
+            
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            return this.Equals(obj as ValidateAddressResponse);
+        }
+
+        /// <summary>
+        /// Returns true if ValidateAddressResponse instances are equal
+        /// </summary>
+        /// <param name="obj">Instance of ValidateAddressResponse to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(ValidateAddressResponse other)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            if (other == null)
+                return false;
+
+            return 
+                (
+                    this.Valid == other.Valid ||
+                    this.Valid != null &&
+                    this.Valid.Equals(other.Valid)
+                ) && 
+                (
+                    this.Response == other.Response ||
+                    this.Response != null &&
+                    this.Response.Equals(other.Response)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks etc, of course :)
+                
+                if (this.Valid != null)
+                    hash = hash * 57 + this.Valid.GetHashCode();
+                
+                if (this.Response != null)
+                    hash = hash * 57 + this.Response.GetHashCode();
+                
+                return hash;
+            }
+        }
+
+    }
+
+
+}
