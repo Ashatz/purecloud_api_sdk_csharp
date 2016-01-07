60d3ee5a6f63962368647ab89df9c13c6cc5ef02
InIn Devops <devops-cloudapplications@inin.com>
2016-01-07 03:14:14 -0500

    1.0.0.86

---
+++ build/1.0.0.86/src/main/csharp/ININ/PureCloudApi/Model/DateTimeField.cs
@@ -0,0 +1,243 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace ININ.PureCloudApi.Model
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class DateTimeField :  IEquatable<DateTimeField>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DateTimeField" /> class.
+        /// </summary>
+        public DateTimeField()
+        {
+            this.Lenient = false;
+            this.Supported = false;
+            
+        }
+
+        
+        /// <summary>
+        /// Gets or Sets Lenient
+        /// </summary>
+        [DataMember(Name="lenient", EmitDefaultValue=false)]
+        public bool? Lenient { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets DurationField
+        /// </summary>
+        [DataMember(Name="durationField", EmitDefaultValue=false)]
+        public DurationField DurationField { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets RangeDurationField
+        /// </summary>
+        [DataMember(Name="rangeDurationField", EmitDefaultValue=false)]
+        public DurationField RangeDurationField { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets LeapDurationField
+        /// </summary>
+        [DataMember(Name="leapDurationField", EmitDefaultValue=false)]
+        public DurationField LeapDurationField { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets MinimumValue
+        /// </summary>
+        [DataMember(Name="minimumValue", EmitDefaultValue=false)]
+        public int? MinimumValue { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets MaximumValue
+        /// </summary>
+        [DataMember(Name="maximumValue", EmitDefaultValue=false)]
+        public int? MaximumValue { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Name
+        /// </summary>
+        [DataMember(Name="name", EmitDefaultValue=false)]
+        public string Name { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Type
+        /// </summary>
+        [DataMember(Name="type", EmitDefaultValue=false)]
+        public DateTimeFieldType Type { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Supported
+        /// </summary>
+        [DataMember(Name="supported", EmitDefaultValue=false)]
+        public bool? Supported { get; set; }
+  
+        
+  
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class DateTimeField {\n");
+            sb.Append("  Lenient: ").Append(Lenient).Append("\n");
+            sb.Append("  DurationField: ").Append(DurationField).Append("\n");
+            sb.Append("  RangeDurationField: ").Append(RangeDurationField).Append("\n");
+            sb.Append("  LeapDurationField: ").Append(LeapDurationField).Append("\n");
+            sb.Append("  MinimumValue: ").Append(MinimumValue).Append("\n");
+            sb.Append("  MaximumValue: ").Append(MaximumValue).Append("\n");
+            sb.Append("  Name: ").Append(Name).Append("\n");
+            sb.Append("  Type: ").Append(Type).Append("\n");
+            sb.Append("  Supported: ").Append(Supported).Append("\n");
+            
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            return this.Equals(obj as DateTimeField);
+        }
+
+        /// <summary>
+        /// Returns true if DateTimeField instances are equal
+        /// </summary>
+        /// <param name="obj">Instance of DateTimeField to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(DateTimeField other)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            if (other == null)
+                return false;
+
+            return 
+                (
+                    this.Lenient == other.Lenient ||
+                    this.Lenient != null &&
+                    this.Lenient.Equals(other.Lenient)
+                ) && 
+                (
+                    this.DurationField == other.DurationField ||
+                    this.DurationField != null &&
+                    this.DurationField.Equals(other.DurationField)
+                ) && 
+                (
+                    this.RangeDurationField == other.RangeDurationField ||
+                    this.RangeDurationField != null &&
+                    this.RangeDurationField.Equals(other.RangeDurationField)
+                ) && 
+                (
+                    this.LeapDurationField == other.LeapDurationField ||
+                    this.LeapDurationField != null &&
+                    this.LeapDurationField.Equals(other.LeapDurationField)
+                ) && 
+                (
+                    this.MinimumValue == other.MinimumValue ||
+                    this.MinimumValue != null &&
+                    this.MinimumValue.Equals(other.MinimumValue)
+                ) && 
+                (
+                    this.MaximumValue == other.MaximumValue ||
+                    this.MaximumValue != null &&
+                    this.MaximumValue.Equals(other.MaximumValue)
+                ) && 
+                (
+                    this.Name == other.Name ||
+                    this.Name != null &&
+                    this.Name.Equals(other.Name)
+                ) && 
+                (
+                    this.Type == other.Type ||
+                    this.Type != null &&
+                    this.Type.Equals(other.Type)
+                ) && 
+                (
+                    this.Supported == other.Supported ||
+                    this.Supported != null &&
+                    this.Supported.Equals(other.Supported)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks etc, of course :)
+                
+                if (this.Lenient != null)
+                    hash = hash * 57 + this.Lenient.GetHashCode();
+                
+                if (this.DurationField != null)
+                    hash = hash * 57 + this.DurationField.GetHashCode();
+                
+                if (this.RangeDurationField != null)
+                    hash = hash * 57 + this.RangeDurationField.GetHashCode();
+                
+                if (this.LeapDurationField != null)
+                    hash = hash * 57 + this.LeapDurationField.GetHashCode();
+                
+                if (this.MinimumValue != null)
+                    hash = hash * 57 + this.MinimumValue.GetHashCode();
+                
+                if (this.MaximumValue != null)
+                    hash = hash * 57 + this.MaximumValue.GetHashCode();
+                
+                if (this.Name != null)
+                    hash = hash * 57 + this.Name.GetHashCode();
+                
+                if (this.Type != null)
+                    hash = hash * 57 + this.Type.GetHashCode();
+                
+                if (this.Supported != null)
+                    hash = hash * 57 + this.Supported.GetHashCode();
+                
+                return hash;
+            }
+        }
+
+    }
+
+
+}
---
+++ build/1.0.0.86/src/main/csharp/ININ/PureCloudApi/Model/Segment.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace ININ.PureCloudApi.Model
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class Segment :  IEquatable<Segment>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Segment" /> class.
+        /// </summary>
+        public Segment()
+        {
+            
+        }
+
+        
+        /// <summary>
+        /// Gets or Sets StartTime
+        /// </summary>
+        [DataMember(Name="startTime", EmitDefaultValue=false)]
+        public DateTime? StartTime { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets EndTime
+        /// </summary>
+        [DataMember(Name="endTime", EmitDefaultValue=false)]
+        public DateTime? EndTime { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Type
+        /// </summary>
+        [DataMember(Name="type", EmitDefaultValue=false)]
+        public string Type { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets HowEnded
+        /// </summary>
+        [DataMember(Name="howEnded", EmitDefaultValue=false)]
+        public string HowEnded { get; set; }
+  
+        
+  
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class Segment {\n");
+            sb.Append("  StartTime: ").Append(StartTime).Append("\n");
+            sb.Append("  EndTime: ").Append(EndTime).Append("\n");
+            sb.Append("  Type: ").Append(Type).Append("\n");
+            sb.Append("  HowEnded: ").Append(HowEnded).Append("\n");
+            
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            return this.Equals(obj as Segment);
+        }
+
+        /// <summary>
+        /// Returns true if Segment instances are equal
+        /// </summary>
+        /// <param name="obj">Instance of Segment to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(Segment other)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            if (other == null)
+                return false;
+
+            return 
+                (
+                    this.StartTime == other.StartTime ||
+                    this.StartTime != null &&
+                    this.StartTime.Equals(other.StartTime)
+                ) && 
+                (
+                    this.EndTime == other.EndTime ||
+                    this.EndTime != null &&
+                    this.EndTime.Equals(other.EndTime)
+                ) && 
+                (
+                    this.Type == other.Type ||
+                    this.Type != null &&
+                    this.Type.Equals(other.Type)
+                ) && 
+                (
+                    this.HowEnded == other.HowEnded ||
+                    this.HowEnded != null &&
+                    this.HowEnded.Equals(other.HowEnded)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks etc, of course :)
+                
+                if (this.StartTime != null)
+                    hash = hash * 57 + this.StartTime.GetHashCode();
+                
+                if (this.EndTime != null)
+                    hash = hash * 57 + this.EndTime.GetHashCode();
+                
+                if (this.Type != null)
+                    hash = hash * 57 + this.Type.GetHashCode();
+                
+                if (this.HowEnded != null)
+                    hash = hash * 57 + this.HowEnded.GetHashCode();
+                
+                return hash;
+            }
+        }
+
+    }
+
+
+}
