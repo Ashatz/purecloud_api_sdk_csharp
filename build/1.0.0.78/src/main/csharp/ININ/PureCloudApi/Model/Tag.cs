03cd8410e40cc7d83a78922150d7bfd6cbee2c81
InIn Devops <devops-cloudapplications@inin.com>
2016-01-07 01:13:48 -0500

    1.0.0.78

---
+++ build/1.0.0.78/src/main/csharp/ININ/PureCloudApi/Model/Tag.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace ININ.PureCloudApi.Model
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class Tag :  IEquatable<Tag>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Tag" /> class.
+        /// </summary>
+        public Tag()
+        {
+            this.InteractionFlag = false;
+            
+        }
+
+        
+        /// <summary>
+        /// Gets or Sets Label
+        /// </summary>
+        [DataMember(Name="label", EmitDefaultValue=false)]
+        public string Label { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets InteractionFlag
+        /// </summary>
+        [DataMember(Name="interactionFlag", EmitDefaultValue=false)]
+        public bool? InteractionFlag { get; set; }
+  
+        
+  
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class Tag {\n");
+            sb.Append("  Label: ").Append(Label).Append("\n");
+            sb.Append("  InteractionFlag: ").Append(InteractionFlag).Append("\n");
+            
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            return this.Equals(obj as Tag);
+        }
+
+        /// <summary>
+        /// Returns true if Tag instances are equal
+        /// </summary>
+        /// <param name="obj">Instance of Tag to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(Tag other)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            if (other == null)
+                return false;
+
+            return 
+                (
+                    this.Label == other.Label ||
+                    this.Label != null &&
+                    this.Label.Equals(other.Label)
+                ) && 
+                (
+                    this.InteractionFlag == other.InteractionFlag ||
+                    this.InteractionFlag != null &&
+                    this.InteractionFlag.Equals(other.InteractionFlag)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks etc, of course :)
+                
+                if (this.Label != null)
+                    hash = hash * 57 + this.Label.GetHashCode();
+                
+                if (this.InteractionFlag != null)
+                    hash = hash * 57 + this.InteractionFlag.GetHashCode();
+                
+                return hash;
+            }
+        }
+
+    }
+
+
+}
