85c89b041ba023cd4f3dbe1e3efb8678c797c460
InIn Devops <devops-cloudapplications@inin.com>
2016-01-06 22:58:45 -0500

    1.0.0.69

---
+++ build/1.0.0.69/src/main/csharp/ININ/PureCloudApi/Model/OrganizationPresence.cs
@@ -0,0 +1,260 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace ININ.PureCloudApi.Model
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class OrganizationPresence :  IEquatable<OrganizationPresence>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrganizationPresence" /> class.
+        /// </summary>
+        public OrganizationPresence()
+        {
+            this.Deactivated = false;
+            
+        }
+
+        
+        /// <summary>
+        /// The globally unique identifier for the object.
+        /// </summary>
+        /// <value>The globally unique identifier for the object.</value>
+        [DataMember(Name="id", EmitDefaultValue=false)]
+        public string Id { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Name
+        /// </summary>
+        [DataMember(Name="name", EmitDefaultValue=false)]
+        public string Name { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets LanguageLabels
+        /// </summary>
+        [DataMember(Name="languageLabels", EmitDefaultValue=false)]
+        public Dictionary<string, string> LanguageLabels { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets SystemPresence
+        /// </summary>
+        [DataMember(Name="systemPresence", EmitDefaultValue=false)]
+        public string SystemPresence { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Deactivated
+        /// </summary>
+        [DataMember(Name="deactivated", EmitDefaultValue=false)]
+        public bool? Deactivated { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets CreatedBy
+        /// </summary>
+        [DataMember(Name="createdBy", EmitDefaultValue=false)]
+        public User CreatedBy { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets CreatedDate
+        /// </summary>
+        [DataMember(Name="createdDate", EmitDefaultValue=false)]
+        public DateTime? CreatedDate { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets ModifiedBy
+        /// </summary>
+        [DataMember(Name="modifiedBy", EmitDefaultValue=false)]
+        public User ModifiedBy { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets ModifiedDate
+        /// </summary>
+        [DataMember(Name="modifiedDate", EmitDefaultValue=false)]
+        public DateTime? ModifiedDate { get; set; }
+  
+        
+        /// <summary>
+        /// The URI for this object
+        /// </summary>
+        /// <value>The URI for this object</value>
+        [DataMember(Name="selfUri", EmitDefaultValue=false)]
+        public string SelfUri { get; set; }
+  
+        
+  
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class OrganizationPresence {\n");
+            sb.Append("  Id: ").Append(Id).Append("\n");
+            sb.Append("  Name: ").Append(Name).Append("\n");
+            sb.Append("  LanguageLabels: ").Append(LanguageLabels).Append("\n");
+            sb.Append("  SystemPresence: ").Append(SystemPresence).Append("\n");
+            sb.Append("  Deactivated: ").Append(Deactivated).Append("\n");
+            sb.Append("  CreatedBy: ").Append(CreatedBy).Append("\n");
+            sb.Append("  CreatedDate: ").Append(CreatedDate).Append("\n");
+            sb.Append("  ModifiedBy: ").Append(ModifiedBy).Append("\n");
+            sb.Append("  ModifiedDate: ").Append(ModifiedDate).Append("\n");
+            sb.Append("  SelfUri: ").Append(SelfUri).Append("\n");
+            
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            return this.Equals(obj as OrganizationPresence);
+        }
+
+        /// <summary>
+        /// Returns true if OrganizationPresence instances are equal
+        /// </summary>
+        /// <param name="obj">Instance of OrganizationPresence to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(OrganizationPresence other)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            if (other == null)
+                return false;
+
+            return 
+                (
+                    this.Id == other.Id ||
+                    this.Id != null &&
+                    this.Id.Equals(other.Id)
+                ) && 
+                (
+                    this.Name == other.Name ||
+                    this.Name != null &&
+                    this.Name.Equals(other.Name)
+                ) && 
+                (
+                    this.LanguageLabels == other.LanguageLabels ||
+                    this.LanguageLabels != null &&
+                    this.LanguageLabels.SequenceEqual(other.LanguageLabels)
+                ) && 
+                (
+                    this.SystemPresence == other.SystemPresence ||
+                    this.SystemPresence != null &&
+                    this.SystemPresence.Equals(other.SystemPresence)
+                ) && 
+                (
+                    this.Deactivated == other.Deactivated ||
+                    this.Deactivated != null &&
+                    this.Deactivated.Equals(other.Deactivated)
+                ) && 
+                (
+                    this.CreatedBy == other.CreatedBy ||
+                    this.CreatedBy != null &&
+                    this.CreatedBy.Equals(other.CreatedBy)
+                ) && 
+                (
+                    this.CreatedDate == other.CreatedDate ||
+                    this.CreatedDate != null &&
+                    this.CreatedDate.Equals(other.CreatedDate)
+                ) && 
+                (
+                    this.ModifiedBy == other.ModifiedBy ||
+                    this.ModifiedBy != null &&
+                    this.ModifiedBy.Equals(other.ModifiedBy)
+                ) && 
+                (
+                    this.ModifiedDate == other.ModifiedDate ||
+                    this.ModifiedDate != null &&
+                    this.ModifiedDate.Equals(other.ModifiedDate)
+                ) && 
+                (
+                    this.SelfUri == other.SelfUri ||
+                    this.SelfUri != null &&
+                    this.SelfUri.Equals(other.SelfUri)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks etc, of course :)
+                
+                if (this.Id != null)
+                    hash = hash * 57 + this.Id.GetHashCode();
+                
+                if (this.Name != null)
+                    hash = hash * 57 + this.Name.GetHashCode();
+                
+                if (this.LanguageLabels != null)
+                    hash = hash * 57 + this.LanguageLabels.GetHashCode();
+                
+                if (this.SystemPresence != null)
+                    hash = hash * 57 + this.SystemPresence.GetHashCode();
+                
+                if (this.Deactivated != null)
+                    hash = hash * 57 + this.Deactivated.GetHashCode();
+                
+                if (this.CreatedBy != null)
+                    hash = hash * 57 + this.CreatedBy.GetHashCode();
+                
+                if (this.CreatedDate != null)
+                    hash = hash * 57 + this.CreatedDate.GetHashCode();
+                
+                if (this.ModifiedBy != null)
+                    hash = hash * 57 + this.ModifiedBy.GetHashCode();
+                
+                if (this.ModifiedDate != null)
+                    hash = hash * 57 + this.ModifiedDate.GetHashCode();
+                
+                if (this.SelfUri != null)
+                    hash = hash * 57 + this.SelfUri.GetHashCode();
+                
+                return hash;
+            }
+        }
+
+    }
+
+
+}
