a68c4391c52a1bd1fea9bc06f266b416cd844350
InIn Devops <devops-cloudapplications@inin.com>
2016-01-07 00:58:53 -0500

    1.0.0.77

---
+++ build/1.0.0.77/src/main/csharp/ININ/PureCloudApi/Model/DomainLicense.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace ININ.PureCloudApi.Model
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class DomainLicense :  IEquatable<DomainLicense>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DomainLicense" /> class.
+        /// </summary>
+        public DomainLicense()
+        {
+            
+        }
+
+        
+        /// <summary>
+        /// The globally unique identifier for the object.
+        /// </summary>
+        /// <value>The globally unique identifier for the object.</value>
+        [DataMember(Name="id", EmitDefaultValue=false)]
+        public string Id { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Name
+        /// </summary>
+        [DataMember(Name="name", EmitDefaultValue=false)]
+        public string Name { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Description
+        /// </summary>
+        [DataMember(Name="description", EmitDefaultValue=false)]
+        public string Description { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Product
+        /// </summary>
+        [DataMember(Name="product", EmitDefaultValue=false)]
+        public string Product { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets Permissions
+        /// </summary>
+        [DataMember(Name="permissions", EmitDefaultValue=false)]
+        public List<string> Permissions { get; set; }
+  
+        
+        /// <summary>
+        /// The URI for this object
+        /// </summary>
+        /// <value>The URI for this object</value>
+        [DataMember(Name="selfUri", EmitDefaultValue=false)]
+        public string SelfUri { get; set; }
+  
+        
+  
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class DomainLicense {\n");
+            sb.Append("  Id: ").Append(Id).Append("\n");
+            sb.Append("  Name: ").Append(Name).Append("\n");
+            sb.Append("  Description: ").Append(Description).Append("\n");
+            sb.Append("  Product: ").Append(Product).Append("\n");
+            sb.Append("  Permissions: ").Append(Permissions).Append("\n");
+            sb.Append("  SelfUri: ").Append(SelfUri).Append("\n");
+            
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            return this.Equals(obj as DomainLicense);
+        }
+
+        /// <summary>
+        /// Returns true if DomainLicense instances are equal
+        /// </summary>
+        /// <param name="obj">Instance of DomainLicense to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(DomainLicense other)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            if (other == null)
+                return false;
+
+            return 
+                (
+                    this.Id == other.Id ||
+                    this.Id != null &&
+                    this.Id.Equals(other.Id)
+                ) && 
+                (
+                    this.Name == other.Name ||
+                    this.Name != null &&
+                    this.Name.Equals(other.Name)
+                ) && 
+                (
+                    this.Description == other.Description ||
+                    this.Description != null &&
+                    this.Description.Equals(other.Description)
+                ) && 
+                (
+                    this.Product == other.Product ||
+                    this.Product != null &&
+                    this.Product.Equals(other.Product)
+                ) && 
+                (
+                    this.Permissions == other.Permissions ||
+                    this.Permissions != null &&
+                    this.Permissions.SequenceEqual(other.Permissions)
+                ) && 
+                (
+                    this.SelfUri == other.SelfUri ||
+                    this.SelfUri != null &&
+                    this.SelfUri.Equals(other.SelfUri)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks etc, of course :)
+                
+                if (this.Id != null)
+                    hash = hash * 57 + this.Id.GetHashCode();
+                
+                if (this.Name != null)
+                    hash = hash * 57 + this.Name.GetHashCode();
+                
+                if (this.Description != null)
+                    hash = hash * 57 + this.Description.GetHashCode();
+                
+                if (this.Product != null)
+                    hash = hash * 57 + this.Product.GetHashCode();
+                
+                if (this.Permissions != null)
+                    hash = hash * 57 + this.Permissions.GetHashCode();
+                
+                if (this.SelfUri != null)
+                    hash = hash * 57 + this.SelfUri.GetHashCode();
+                
+                return hash;
+            }
+        }
+
+    }
+
+
+}
---
+++ build/1.0.0.77/src/main/csharp/ININ/PureCloudApi/Model/MediaSetting.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace ININ.PureCloudApi.Model
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class MediaSetting :  IEquatable<MediaSetting>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaSetting" /> class.
+        /// </summary>
+        public MediaSetting()
+        {
+            
+        }
+
+        
+        /// <summary>
+        /// Gets or Sets AlertingTimeoutSeconds
+        /// </summary>
+        [DataMember(Name="alertingTimeoutSeconds", EmitDefaultValue=false)]
+        public int? AlertingTimeoutSeconds { get; set; }
+  
+        
+        /// <summary>
+        /// Gets or Sets ServiceLevel
+        /// </summary>
+        [DataMember(Name="serviceLevel", EmitDefaultValue=false)]
+        public ServiceLevel ServiceLevel { get; set; }
+  
+        
+  
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class MediaSetting {\n");
+            sb.Append("  AlertingTimeoutSeconds: ").Append(AlertingTimeoutSeconds).Append("\n");
+            sb.Append("  ServiceLevel: ").Append(ServiceLevel).Append("\n");
+            
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            return this.Equals(obj as MediaSetting);
+        }
+
+        /// <summary>
+        /// Returns true if MediaSetting instances are equal
+        /// </summary>
+        /// <param name="obj">Instance of MediaSetting to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(MediaSetting other)
+        {
+            // credit: http://stackoverflow.com/a/10454552/677735
+            if (other == null)
+                return false;
+
+            return 
+                (
+                    this.AlertingTimeoutSeconds == other.AlertingTimeoutSeconds ||
+                    this.AlertingTimeoutSeconds != null &&
+                    this.AlertingTimeoutSeconds.Equals(other.AlertingTimeoutSeconds)
+                ) && 
+                (
+                    this.ServiceLevel == other.ServiceLevel ||
+                    this.ServiceLevel != null &&
+                    this.ServiceLevel.Equals(other.ServiceLevel)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks etc, of course :)
+                
+                if (this.AlertingTimeoutSeconds != null)
+                    hash = hash * 57 + this.AlertingTimeoutSeconds.GetHashCode();
+                
+                if (this.ServiceLevel != null)
+                    hash = hash * 57 + this.ServiceLevel.GetHashCode();
+                
+                return hash;
+            }
+        }
+
+    }
+
+
+}
