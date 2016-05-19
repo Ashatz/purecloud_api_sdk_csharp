1691e85cab16fce186243e2f05c703019c4c646a
InIn Devops <devops-jenkins@inin.com>
2016-05-19 18:42:22 -0400

    0.44.0.267

--- build/src/main/csharp/ININ.PureCloudApi/Client/Configuration.cs
+++ build/src/main/csharp/ININ.PureCloudApi/Client/Configuration.cs
@@ -36,7 +36,7 @@ namespace ININ.PureCloudApi.Client
                              string tempFolderPath = null,
                              string dateTimeFormat = null,
                              int timeout = 100000,
-                             string userAgent = "Swagger-Codegen/0.43.0.265/csharp"
+                             string userAgent = "Swagger-Codegen/0.44.0.267/csharp"
                             )
         {
             setApiClientUsingDefault(apiClient);
@@ -71,7 +71,7 @@ namespace ININ.PureCloudApi.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "0.43.0.265";
+        public const string Version = "0.44.0.267";
 
         /// <summary>
         /// Gets or sets the default Configuration.
@@ -276,7 +276,7 @@ namespace ININ.PureCloudApi.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
             report += "    Version of the API: v2\n";
-            report += "    SDK Package Version: 0.43.0.265\n";
+            report += "    SDK Package Version: 0.44.0.267\n";
 
             return report;
         }
--- build/src/main/csharp/ININ.PureCloudApi/Properties/AssemblyInfo.cs
+++ build/src/main/csharp/ININ.PureCloudApi/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.43.0.265")]
-[assembly: AssemblyFileVersion("0.43.0.265")]
+[assembly: AssemblyVersion("0.44.0.267")]
+[assembly: AssemblyFileVersion("0.44.0.267")]
