4f983909c6f28407ce91d593d360a5ba3b3d564c
unknown <Tim.Smith@notamac.i3domain.inin.com>
2016-08-03 09:42:58 -0600

    API-1804 Adding custom serializer

---
+++ Extensions/Client/Iso8601DateTimeConverter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace ININ.PureCloudApi.Client
+{
+    public class Iso8601DateTimeConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(DateTime)) || (objectType == typeof(DateTime?));
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var date = (DateTime?)value;
+            if (!date.HasValue)
+                writer.WriteNull();
+            else
+            {
+                var dateString = date.Value.ToUniversalTime().ToString("s", CultureInfo.InvariantCulture);
+                writer.WriteValue(dateString);
+            }
+        }
+
+        public override bool CanRead => false;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
