103be3d054edcf5ac169f158babb0bd1a433c90c
InIn Devops <devops-jenkins@inin.com>
2016-08-16 12:02:44 +0000

    0.72.7.351

--- build/src/ININ.PureCloudApi/Properties/AssemblyInfo.cs
+++ build/src/ININ.PureCloudApi/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.72.6.350")]
-[assembly: AssemblyFileVersion("0.72.6.350")]
+[assembly: AssemblyVersion("0.72.7.351")]
+[assembly: AssemblyFileVersion("0.72.7.351")]
