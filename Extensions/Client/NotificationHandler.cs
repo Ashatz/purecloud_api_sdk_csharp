572e6e7f904556b9e31eecd3f1070898e1961bcb
Smith, Tim <Tim.Smith@inin.com>
2016-08-17 12:07:14 -0600

    API-1752 adding notifications to swagger

--- Extensions/Client/NotificationHandler.cs
+++ Extensions/Client/NotificationHandler.cs
@@ -63,8 +63,6 @@ namespace ININ.PureCloudApi.Client
         /// <param name="type">The <see cref="Type"/> to cast notifications on this topic to</param>
         public void AddSubscription(string topic, Type type)
         {
-            if (!type.IsAssignableFrom(typeof (INotificationData)))
-                throw new InvalidNotificationTypeException(type);
             _notificationsApi.PostChannelsChannelIdSubscriptions(Channel.Id,
                 new List<ChannelTopic>() { new ChannelTopic(topic) });
             _typeMap.Add(topic.ToLowerInvariant(), type);
