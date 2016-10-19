52da2a175a3ecaf60d44dc0420e91c078e59a5ba
InIn Devops <devops-jenkins@inin.com>
2016-10-19 16:28:47 +0000

    0.89.11.383

--- build/src/ININ.PureCloudApi/Client/Configuration.cs
+++ build/src/ININ.PureCloudApi/Client/Configuration.cs
@@ -36,7 +36,7 @@ namespace ININ.PureCloudApi.Client
                              string tempFolderPath = null,
                              string dateTimeFormat = null,
                              int timeout = 100000,
-                             string userAgent = "PureCloud SDK/0.89.10.382/csharp"
+                             string userAgent = "PureCloud SDK/0.89.11.383/csharp"
                             )
         {
             setApiClientUsingDefault(apiClient);
@@ -71,7 +71,7 @@ namespace ININ.PureCloudApi.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "0.89.10.382";
+        public const string Version = "0.89.11.383";
 
         /// <summary>
         /// Gets or sets the default Configuration.
@@ -276,7 +276,7 @@ namespace ININ.PureCloudApi.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
             report += "    Version of the API: v2\n";
-            report += "    SDK Package Version: 0.89.10.382\n";
+            report += "    SDK Package Version: 0.89.11.383\n";
 
             return report;
         }
