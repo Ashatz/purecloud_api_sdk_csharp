baf24a8d81b806da8fe33491c17cb98718eb92e8
InIn Devops <devops-jenkins@inin.com>
2017-01-18 21:57:03 +0000

    0.100.16.29

--- build/src/ININ.PureCloudApi/Client/Configuration.cs
+++ build/src/ININ.PureCloudApi/Client/Configuration.cs
@@ -36,7 +36,7 @@ namespace ININ.PureCloudApi.Client
                              string tempFolderPath = null,
                              string dateTimeFormat = null,
                              int timeout = 100000,
-                             string userAgent = "PureCloud SDK/0.100.15.28/csharp"
+                             string userAgent = "PureCloud SDK/0.100.16.29/csharp"
                             )
         {
             setApiClientUsingDefault(apiClient);
@@ -71,7 +71,7 @@ namespace ININ.PureCloudApi.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "0.100.15.28";
+        public const string Version = "0.100.16.29";
 
         /// <summary>
         /// Gets or sets the default Configuration.
@@ -276,7 +276,7 @@ namespace ININ.PureCloudApi.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
             report += "    Version of the API: v2\n";
-            report += "    SDK Package Version: 0.100.15.28\n";
+            report += "    SDK Package Version: 0.100.16.29\n";
 
             return report;
         }
--- build/src/ININ.PureCloudApi/Properties/AssemblyInfo.cs
+++ build/src/ININ.PureCloudApi/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.100.15.28")]
-[assembly: AssemblyFileVersion("0.100.15.28")]
+[assembly: AssemblyVersion("0.100.16.29")]
+[assembly: AssemblyFileVersion("0.100.16.29")]
